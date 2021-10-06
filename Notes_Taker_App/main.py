06219971e793e80c2780f3223400a53b75c2ea56
Ramkumar2312 <ramkumars.2323@gmail.com>
2021-10-06 12:45:40 +0530

    changed user authentication using flask-jwt-extended

--- Notes_Taker_App/main.py
+++ Notes_Taker_App/main.py
@@ -1,23 +1,29 @@
 from flask import Flask,request
 from flask import jsonify
 import sqlite3
-from flask_jwt import JWT,jwt_required
+# from flask_jwt import JWT,jwt_required
+
+from flask_jwt_extended import JWTManager,jwt_required,get_jwt_identity,create_access_token
 from security import authenticate,identity
 
 app = Flask(__name__)
 app.secret_key = "1234qwerty"
 
-jwt = JWT(app,authenticate,identity)
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+# jwt = JWT(app,authenticate,identity)
 
 @app.route('/')
 def home():
     return "Hello world !"
 
 @app.route('/add',methods=['POST'])
+@jwt_required()
 def add_data():
     data = request.get_json()
     note = data['note']
-    user_id = data['user_id']
+    current_user = get_jwt_identity()
+    user_id = current_user
     connection = sqlite3.connect('notes.db')
     cursor = connection.cursor()
     cursor.execute("PRAGMA foreign_keys = ON")
@@ -36,14 +42,16 @@ def add_data():
 
 
 @app.route('/display',methods=['GET'])
-# @jwt_required()
+@jwt_required()
 def display_data():
     connection = sqlite3.connect('notes.db')
     cursor = connection.cursor()
     cursor.execute("PRAGMA foreign_keys = ON")
+    current_user = get_jwt_identity()
+    print(current_user)
     try :
-        select_all = "SELECT * FROM notes"
-        cursor.execute(select_all)
+        select_all = "SELECT * FROM notes WHERE user_id=?"
+        cursor.execute(select_all,(current_user,))
     except Exception as e:
         cursor.close()
         connection.close()
@@ -59,10 +67,12 @@ def display_data():
 
 
 @app.route('/update',methods=['PUT'])
+@jwt_required()
 def update_data():
     data = request.get_json()
     _id = data['id']
     note = data['note']
+
     connection = sqlite3.connect('notes.db')
     cursor = connection.cursor()
     cursor.execute("PRAGMA foreign_keys = ON")
@@ -80,10 +90,12 @@ def update_data():
 
 
 @app.route('/delete',methods=['DELETE'])
+@jwt_required()
 def delete_data():
     data = request.get_json()
+    current_user = get_jwt_identity()
     _id = data['id']
-    user_id = data['user_id']
+    user_id = current_user
     connection = sqlite3.connect('notes.db')
     cursor = connection.cursor()
     cursor.execute("PRAGMA foreign_keys = ON")
@@ -158,6 +170,27 @@ def delete_user():
     cursor.close()
     connection.close()
     return jsonify({"message":"User deleted"})
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    connection = sqlite3.connect('notes.db')
+    cursor = connection.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+
+    try :
+        select_user = "SELECT * FROM users WHERE username=? and password=?"
+        user = cursor.execute(select_user,(data['username'], data['password'])).fetchone()
+        user_id = user[0]
+    except Exception as e:
+        cursor.close()
+        connection.close()
+        return jsonify({'message': e.args})
+
+    if user is not None:
+
+        access_token = create_access_token(identity=user_id)
+        return jsonify(access_token=access_token)
 
 if __name__ == '__main__':
         app.run(port=5007,debug=True)
